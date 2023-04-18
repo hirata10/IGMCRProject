aa520fac32f397d9a2e17555aac2f433c99ef3d8
Yuanyuan Yang <1005842457@qq.com>
2023-04-18 06:26:44 -0700

    Update IGM.py

--- IGM.py
+++ IGM.py
@@ -62,6 +62,17 @@ E_minus[0] = E_minus[1] / (E[1] / E[0])
 
 # In[3]:
 
+"""
+To use this class, call IGM_N(deltalin0, model)
+where deltalin0 is overdensity factor (<0 means underdensity, 0 means mean density, >0 means overdensity),
+and model is either 0 or 1 (0 using Khaire's source model and 1 using Haardt's source model)
+
+all variables should be individual values rather than array or list
+
+.get_utot() will return to the energy density utot and energy per baryon in eV E_eV
+.get_P() will return to the number of electron per volume per erg N, pressure in erg cm^-3 P_e and pressure in eV cm^-3 P_eV
+"""
+
 
 class IGM_N:
     def __init__(self, deltalin0, model):
